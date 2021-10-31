856bfc6dfce5a33df7493b083811a805dbf7c766
seol <suljiwon@naver.com>
2021-10-31 23:48:58 +0900

    feat: create api/blog/post routing

---
+++ assignment2/level2/practice/routes/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.use("/api", require("./api"));
+router.use("/api/blog", require("./api/blog"));
+router.use("/api/blog/post", require("./api/blog/post"));
+
+module.exports = router;
